fbcc5edd76075f59f4aeb369df9a60edb445beb3
Patrick Wong <wp.patrickwong@gmail.com>
2026-03-25 23:01:15 -0700

    chore: remove dist-electron

--- frontend/dist-electron/preload.js
+++
@@ -1,7 +0,0 @@
-import { contextBridge, ipcRenderer } from "electron";
-//#region electron/preload.ts
-contextBridge.exposeInMainWorld("electron", {
-	send: (channel, data) => ipcRenderer.send(channel, data),
-	on: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args))
-});
-//#endregion
